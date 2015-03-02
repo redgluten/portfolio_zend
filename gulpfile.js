83e96b62c4b51a65b61237ed92945b7cdb71dddb
gluten <goulvenschaal@me.com>
2015-03-02 14:12:42 +0100

    frontend dependances

---
+++ gulpfile.js
@@ -0,0 +1,144 @@
+// Modules
+// =======
+
+// General
+var gulp           = require('gulp');
+var gulpif         = require('gulp-if');
+// Parse CLI commands, used for dev & prod arguments
+var minimist       = require('minimist');
+// Gives nice notifications
+var notify         = require('gulp-notify');
+// Reload browser or inject styles on change
+var livereload     = require('gulp-livereload');
+// Filter files
+var filter         = require('gulp-filter');
+// Concat files into array
+var concat         = require('gulp-concat');
+// Source maps for development
+var sourcemaps     = require('gulp-sourcemaps');
+
+// Styles
+var sass           = require('gulp-sass');
+var autoprefixer   = require('gulp-autoprefixer');
+var minifycss      = require('gulp-minify-css');
+//var sassdoc        = require('sassdoc');
+
+// JavaScript
+var jshint         = require('gulp-jshint');
+var uglify         = require('gulp-uglify');
+
+// Images
+var imagemin       = require('gulp-imagemin');
+
+
+// CLI Options
+// ===========
+var options = minimist(process.argv.slice(2));
+
+
+// Paths
+// =====
+
+// Define default destination folder
+var publicPath = 'src/public/';
+
+// Define assets path
+var assetsPath = 'src/application/assets/';
+
+// Define location of Bower config files
+var bowerPath = 'vendor/bower_components/';
+
+
+// Actions
+// =======
+
+// Stylesheets
+gulp.task('styles', function() {
+    var customStyles = [assetsPath + 'sass/*.scss'];
+
+    // vendor assets may also be called from custom main.scss
+    gulp.src(customStyles)
+        .pipe(sourcemaps.init())
+        //.pipe(sassdoc())
+        .pipe(sass({
+            errLogToConsole: false,
+            onError: function(err) {
+                return notify().write(err.message + ' in ' + err.file + ':' + err.line);
+            }
+        }))
+        .pipe(autoprefixer("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(concat('app.css'))
+        .pipe(gulpif(options.env === 'production', minifycss()))
+        .pipe(sourcemaps.write('maps/'))
+        .pipe(gulp.dest(publicPath + 'css'))
+        .pipe(notify({ message: 'Styles task complete' }))
+        .pipe(livereload());
+});
+
+// JavaScript
+gulp.task('scripts', function() {
+    var jQuery      = [bowerPath + 'jquery/dist/jquery.min.js'];
+    var bootstrapJs = [bowerPath + 'javascripts/bootstrap.js'];
+    var customJs    = [assetsPath + '/js/*.js'];
+
+    // Hints on custom js
+    gulp.src(customJs)
+        .pipe(jshint())
+        // Use gulp-notify as jshint reporter
+        .pipe(notify(function (file) {
+            if (file.jshint.success) return false;
+
+            var errors = file.jshint.results.map(function (data) {
+                if (data.error) {
+                    return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+                }
+            }).join("\n");
+            return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
+        }));
+
+    // Build js from custom + libs
+    gulp.src(jQuery.concat(bootstrapJs).concat(customJs))
+        .pipe(filter('*.js'))
+        .pipe(concat('app.js'))
+        .pipe(gulpif(options.env === 'production', uglify()))
+        .pipe(gulp.dest(publicPath + '/js'))
+        .pipe(notify('JS compiled'))
+        .pipe(livereload());
+});
+
+
+// Images
+gulp.task('images', function() {
+    var customImages  = [assetsPath + '/images/*.{png,gif,jpg}'];
+
+    gulp.src(customImages)
+        .pipe(imagemin())
+        .pipe(gulp.dest(publicPath + '/images'))
+        .pipe(livereload());
+});
+
+// Uploads
+gulp.task('uploads', function()
+{
+    gulp.src(publicPath + '/uploads/*.{png,gif,jpg}')
+        .pipe(imagemin());
+});
+
+// Views
+gulp.task('views', function() {
+    gulp.src('application/views/**/*.php')
+        .pipe(livereload());
+});
+
+// Watch
+gulp.task('watch', function () {
+    livereload.listen();
+
+    gulp.watch(assetsPath + 'sass/**/*.scss', ['styles']);
+    gulp.watch(assetsPath + '/js/**.js', ['scripts']);
+    gulp.watch(assetsPath + '/images/**', ['images']);
+    gulp.watch('application/views/**/*.php', ['views']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch']);
--- src/public/index.php
+++ src/public/index.php
@@ -1,8 +1,14 @@
 <?php
 
+defined('ROOT_PATH')
+	|| define('ROOT_PATH', realpath(dirname(dirname(__DIR__))));
+
+defined('SRC_PATH')
+	|| define('SRC_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'src');
+
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+    || define('APPLICATION_PATH', SRC_PATH . DIRECTORY_SEPARATOR . '/application');
 
 // Define application environment
 defined('APPLICATION_ENV')
@@ -14,8 +20,11 @@ set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
 )));
 
-/** Zend_Application */
-require_once 'Zend/Application.php';
+if (! file_exists(ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor'. DIRECTORY_SEPARATOR . 'autoload.php')) {
+	die('Veuillez lancer la commande "composer install" pour utiliser cette application');
+}
+
+require_once ROOT_PATH . DIRECTORY_SEPARATOR . 'vendor'. DIRECTORY_SEPARATOR . 'autoload.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
